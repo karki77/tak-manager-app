7f95cbbd7f03e6a53f18aefd24664ae7ce31b735
Kuber Karki <59843801+karki77@users.noreply.github.com>
2025-04-15 10:42:46 +0545

    first commit

---
+++ src/routers/router.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client'; // Adjust the import path as necessary
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+// POST route to create a new User
+router.post('/user', async (req, res) => {
+    const { id, email, password } = req.body;
+
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                id,
+                email,
+                password                   
+            },
+        });
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to create user' });
+    }
+});
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import routers from './routers/router';
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+
+
+dotenv.config();
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+
+
+void (async (): Promise<void> => {
+    try {
+      // CONNECT DATABASE
+      await prisma.$connect();
+      
+      console.log("Database connected successfully.");
+
+} catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(`ERROR CONNECTING DATABASE: ${(error as Error).message}`);
+    console.error(error);
+    process.exit(1);
+  }
+})();
+
+app.use(cors());
+app.use(express.json());
+
+// app.get('/test', (req, res) => {
+//     res.send('Hello World!')}
+// );
+app.use('/test', routers);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+    
+});
